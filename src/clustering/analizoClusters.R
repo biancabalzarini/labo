62a58622b96c7f8ac7d03c6172cea6a0202adf62
biancabalzarini <balzarini.bianca@gmail.com>
2022-06-23 12:01:10 -0300

    cluster de 4

--- src/clustering/analizoClusters.R
+++ src/clustering/analizoClusters.R
@@ -6,6 +6,10 @@ gc()             #garbage collection
 
 require("data.table")
 
+
+library("ggplot2")
+library("ggforce")
+
 #7 CLUSTERS, NO HISTORICO
 
 setwd( "C:/Users/Bianca/OneDrive/Documentos/Archivos/ITBA/MineriaDeDatos/labo/expCloud/ST7610" )
@@ -91,7 +95,7 @@ print("Cantidad de clusters:")
 print(length(unique(dataset$cluster2)))
 
 
-~~~
+#~~~#
 
 
 # Dataset original completo
@@ -123,12 +127,108 @@ mes_m2 = data_all[,.SD[which.max(foto_mes)-2],by=numero_de_cliente] # Me quedo (
 
 mes_m3 = data_all[,.SD[which.max(foto_mes)-3],by=numero_de_cliente] # Me quedo (para cada cliente) con la foto de su ante-ante-penúltimo mes antes de morir (mes -3) # Son todos (o casi todos, no se porque podría haber algun colado) CONTINUA
 
+mes_m4 = data_all[,.SD[which.max(foto_mes)-4],by=numero_de_cliente]
+
+mes_m5 = data_all[,.SD[which.max(foto_mes)-5],by=numero_de_cliente]
+
+mes_m6 = data_all[,.SD[which.max(foto_mes)-6],by=numero_de_cliente]
+
+mes_m7 = data_all[,.SD[which.max(foto_mes)-7],by=numero_de_cliente]
+
+mes_m8 = data_all[,.SD[which.max(foto_mes)-8],by=numero_de_cliente]
+
 
 # Ejemplo de un grafico que se puede hacer
-var = quote(ctrx_quarter)
-plot(ultimo_mes[  , mean(eval(var)),  cluster2 ], type="b",ylim=c(0,85)) # El ylim es a mano, tedría que buscar como automatizarlo
+var = quote(mcuentas_saldo)
+
+plot(ultimo_mes[  , mean(eval(var)),  cluster2 ], type="b")#,ylim=c(-70000,40000)) # El ylim es a mano, tedría que buscar como automatizarlo
 lines(mes_m1[  , mean(eval(var)),  cluster2 ], col='red', type = "b")
 lines(mes_m2[  , mean(eval(var)),  cluster2 ], col='green', type = "b")
 lines(mes_m3[  , mean(eval(var)),  cluster2 ], col='blue', type = "b")
-legend("topright", legend=c("Mes 0", "Mes -1", "Mes -2", "Mes -3"),col=c("black", "red", "green", "blue"), lty = 1:2, cex=0.8)
+lines(mes_m4[  , mean(eval(var)),  cluster2 ], col='blue', type = "b")
+lines(mes_m5[  , mean(eval(var)),  cluster2 ], col='blue', type = "b")
+lines(mes_m6[  , mean(eval(var)),  cluster2 ], col='blue', type = "b")
+lines(mes_m7[  , mean(eval(var)),  cluster2 ], col='blue', type = "b")
+lines(mes_m8[  , mean(eval(var)),  cluster2 ], col='blue', type = "b")
+legend("topright", legend=c("Media mes 0", "Media mes -1", "Media mes -2", "Media mes -3", "Media mes -4", "Media mes -5", "Media mes -6", "Media mes -7", "Media mes -8"),col=c("black", "red", "green", "blue", "orange", "violet", "pink", "grey", "yellow"), lty = 1:2, cex=0.8)
 
+plot(ultimo_mes[  , sd(eval(var)),  cluster2 ], type="b")#,ylim=c(50000,600000)) # El ylim es a mano, tedría que buscar como automatizarlo
+lines(mes_m1[  , sd(eval(var)),  cluster2 ], col='red', type = "b")
+lines(mes_m2[  , sd(eval(var)),  cluster2 ], col='green', type = "b")
+lines(mes_m3[  , sd(eval(var)),  cluster2 ], col='blue', type = "b")
+lines(mes_m4[  , sd(eval(var)),  cluster2 ], col='blue', type = "b")
+lines(mes_m5[  , sd(eval(var)),  cluster2 ], col='blue', type = "b")
+lines(mes_m6[  , sd(eval(var)),  cluster2 ], col='blue', type = "b")
+lines(mes_m7[  , sd(eval(var)),  cluster2 ], col='blue', type = "b")
+lines(mes_m8[  , sd(eval(var)),  cluster2 ], col='blue', type = "b")
+legend("topright", legend=c("Desvio mes 0", "Desvio mes -1", "Desvio mes -2", "Desvio mes -3", "Desvio mes -4", "Desvio mes -5", "Desvio mes -6", "Desvio mes -7", "Desvio mes -8"),col=c("black", "red", "green", "blue", "orange", "violet", "pink", "grey", "yellow"), lty = 1:2, cex=0.8)
+
+
+# Otro grafico
+var = quote(cliente_antiguedad)
+
+a = cbind(mes_m8[  , mean(eval(var)),  cluster2 ][,2],
+          mes_m7[  , mean(eval(var)),  cluster2 ][,2],
+          mes_m6[  , mean(eval(var)),  cluster2 ][,2],
+          mes_m5[  , mean(eval(var)),  cluster2 ][,2],
+          mes_m4[  , mean(eval(var)),  cluster2 ][,2],
+          mes_m3[  , mean(eval(var)),  cluster2 ][,2],
+          mes_m2[  , mean(eval(var)),  cluster2 ][,2],
+          mes_m1[  , mean(eval(var)),  cluster2 ][,2],
+          ultimo_mes[  , mean(eval(var)),  cluster2 ][,2])
+
+b = cbind(mes_m8[  , sd(eval(var)),  cluster2 ][,2],
+          mes_m7[  , sd(eval(var)),  cluster2 ][,2],
+          mes_m6[  , sd(eval(var)),  cluster2 ][,2],
+          mes_m5[  , sd(eval(var)),  cluster2 ][,2],
+          mes_m4[  , sd(eval(var)),  cluster2 ][,2],
+          mes_m3[  , sd(eval(var)),  cluster2 ][,2],
+          mes_m2[  , sd(eval(var)),  cluster2 ][,2],
+          mes_m1[  , sd(eval(var)),  cluster2 ][,2],
+          ultimo_mes[  , sd(eval(var)),  cluster2 ][,2])
+
+
+nro = 1 # Numero de cluster
+
+plot(c(-8,-7,-6,-5,-4,-3,-2,-1,0),a[nro])
+
+plot(c(-8,-7,-6,-5,-4,-3,-2,-1,0),b[nro])
+
+data_circle2 <- data.frame(x0 = seq(min(a[nro]),30*max(a[nro]),(30*max(a[nro])-min(a[nro]))/8),    # Create data for multiple circles
+                           y0 = unlist(as.list(a[nro])),
+                           r = unlist(as.list(b[nro])))
+
+ggplot() +
+  geom_circle(data = data_circle2, aes(x0 = x0, y0 = y0, r = r, col = r))
+
+
+# Otro grafico
+var = quote(mcaja_ahorro_dolares) #cpayroll_trx #mpayroll #ctarjeta_visa_trx #chomebanking_trx #mcaja_ahorro_dolares
+
+a = cbind(mes_m8[  , mean(eval(var)),  cluster2 ][,2],
+          mes_m7[  , mean(eval(var)),  cluster2 ][,2],
+          mes_m6[  , mean(eval(var)),  cluster2 ][,2],
+          mes_m5[  , mean(eval(var)),  cluster2 ][,2],
+          mes_m4[  , mean(eval(var)),  cluster2 ][,2],
+          mes_m3[  , mean(eval(var)),  cluster2 ][,2],
+          mes_m2[  , mean(eval(var)),  cluster2 ][,2],
+          mes_m1[  , mean(eval(var)),  cluster2 ][,2],
+          ultimo_mes[  , mean(eval(var)),  cluster2 ][,2])
+
+b = cbind(mes_m8[  , sd(eval(var)),  cluster2 ][,2],
+          mes_m7[  , sd(eval(var)),  cluster2 ][,2],
+          mes_m6[  , sd(eval(var)),  cluster2 ][,2],
+          mes_m5[  , sd(eval(var)),  cluster2 ][,2],
+          mes_m4[  , sd(eval(var)),  cluster2 ][,2],
+          mes_m3[  , sd(eval(var)),  cluster2 ][,2],
+          mes_m2[  , sd(eval(var)),  cluster2 ][,2],
+          mes_m1[  , sd(eval(var)),  cluster2 ][,2],
+          ultimo_mes[  , sd(eval(var)),  cluster2 ][,2])
+
+colors = c("#EB1E2C","blue","green","orange","black","violet","pink")
+nro = 1 # Numero de cluster
+plot(c(-8,-7,-6,-5,-4,-3,-2,-1,0),a[nro],type="l",ylim=c(min(a),max(a)),col=colors[1],xlab='Meses',ylab=var)
+for (val in 1: length(unique(dataset$cluster2))-1)
+{
+  lines(c(-8,-7,-6,-5,-4,-3,-2,-1,0),a[nro+val],ylim=c(min(a),max(a)),col=colors[1+val])
+}
